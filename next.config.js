2370c231114df38b28457401f8529d2ed38259e0
rowthan <12638456+rowthan@users.noreply.github.com>
2022-11-29 00:15:54 +0800

    feat: svg as component

--- next.config.js
+++ next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    }); // 针对 SVG 的处理规则
+
+    return config;
+  }
 }
