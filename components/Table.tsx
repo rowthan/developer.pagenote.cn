fe192a3bf25e2c6ea8b9891912be1c7f6c0f423c
rowthan <12638456+rowthan@users.noreply.github.com>
2022-11-29 00:21:37 +0800

    feat: 通用型表格组件

---
+++ components/Table.tsx
@@ -0,0 +1,166 @@
+import * as React from "react";
+import {ReactElement, useEffect, useRef, useState} from "react";
+import {Pagination} from "@pagenote/shared/lib/@types/database";
+import get from 'lodash/get';
+
+interface Props<T> {
+    list: T[],
+    headLabels: string[]
+    renderTDS: (item: T, index: number) => ReactElement,
+    footerTR?: ReactElement,
+    footerTD?: ReactElement,
+    pagination: Pagination
+    onPaginationChange: (page: number, size: number) => void
+    selectedIds: Set<string>,
+    onSelectIds: (newSelected: Set<string>) => void,
+    primaryKey: string
+}
+
+/**
+ * 通用型table组件，支持分页、批量选择
+ * */
+export default function Table<T>(props: Props<any>) {
+    const {
+        selectedIds = new Set(),
+        onSelectIds,
+        primaryKey,
+        list,
+        renderTDS,
+        footerTR,
+        footerTD,
+        headLabels = [],
+        pagination = {total: 0, limit: 10, page: 0},
+        onPaginationChange
+    } = props;
+    const {total = 0, limit = 10, page = 0} = pagination;
+    const pages = Math.fround(total / limit)
+
+    const [allChecked, setAllChecked] = useState(false)
+    const allCheckRef = useRef<HTMLInputElement>(null);
+
+    useEffect(function () {
+        let allCheckedTemp: boolean;
+        if (selectedIds.size === 0) {
+            allCheckedTemp = false;
+        } else {
+            allCheckedTemp = list.every(function (item) {
+                return selectedIds.has(get(item, primaryKey))
+            })
+        }
+        setAllChecked(allCheckedTemp)
+
+        if (allCheckRef.current) {
+            if (selectedIds.size === 0) {
+                // @ts-ignore
+                allCheckRef.current.indeterminate = undefined;
+            } else {
+                allCheckRef.current.indeterminate = !allCheckedTemp;
+            }
+        }
+    }, [selectedIds, list])
+
+    function toggleSelect(id: string) {
+        if (selectedIds.has(id)) {
+            selectedIds.delete(id)
+        } else {
+            selectedIds.add(id)
+        }
+
+        onSelectIds(new Set(selectedIds))
+    }
+
+    function toggleAll() {
+        if (allChecked) {
+            onSelectIds(new Set())
+        } else {
+            const ids = list.map(function (item) {
+                return get(item, primaryKey)
+            })
+            onSelectIds(new Set(ids))
+        }
+    }
+
+    return (
+        <table className="table table-compact w-full min-h-100 mx-auto">
+            <thead>
+            <tr>
+                <th className='w-4'>
+                    <label>
+                        <input
+                            ref={allCheckRef}
+                            onChange={toggleAll}
+                            checked={allChecked}
+                            type="checkbox" className="checkbox"/>
+                    </label>
+                </th>
+                {
+                    headLabels.map(function (item) {
+                        return <th key={item}>{item}</th>
+                    })
+                }
+            </tr>
+            </thead>
+            <tbody>
+            {
+                list.map(function (item, index) {
+                    return (
+                        <tr key={index}>
+                            <td className=''>
+                                <label>
+                                    <input onChange={() => {
+                                        toggleSelect(get(item, primaryKey))
+                                    }}
+                                           checked={selectedIds.has(get(item, primaryKey))}
+                                           type="checkbox" className="checkbox"/>
+                                </label>
+                            </td>
+                            {renderTDS(item, index)}
+                        </tr>
+
+                    )
+                })
+            }
+            </tbody>
+            <tfoot className='sticky bottom-0'>
+            <tr>
+                <th colSpan={headLabels.length}>
+                    <div className=' btn-group flex  items-center'>
+                        <div className="btn-group">
+                            <button className="btn btn-sm" disabled={page === 0} onClick={() => {
+                                onPaginationChange(page - 1, limit)
+                            }}>«
+                            </button>
+                            {/* TODO 这里改为跳页 */}
+                            <button className="btn btn-sm">{page + 1}</button>
+                            <button className="btn btn-sm" disabled={page >= pages - 1} onClick={() => {
+                                onPaginationChange(page + 1, limit)
+                            }}>»
+                            </button>
+                        </div>
+                        <select value={limit} onChange={(e) => {
+                            onPaginationChange(page, Number(e.target.value))
+                        }} className="ml-2 select select-bordered select-sm">
+                            {
+                                [10, 100, 1000].map(function (value) {
+                                    return (
+                                        <option key={value} value={value}>
+                                            {value}条/每页
+                                        </option>
+                                    )
+                                })
+                            }
+                        </select>
+                        <span className='text-xs text-gray-400 ml-2'>
+                            共计 {total} 条
+                        </span>
+                    </div>
+                </th>
+                <th>
+                    {footerTD}
+                </th>
+            </tr>
+            {footerTR}
+            </tfoot>
+        </table>
+    )
+}
