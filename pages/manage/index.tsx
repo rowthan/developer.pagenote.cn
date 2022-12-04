7ba3e68ecfbaf3299a4c4e280f42f0b98d7da510
rowthan <12638456+rowthan@users.noreply.github.com>
2022-12-04 18:52:23 +0800

    fix: build error

--- pages/manage/index.tsx
+++ pages/manage/index.tsx
@@ -1,7 +1,3 @@
-import {useEffect, useState} from "react";
-import {WebPage} from "@pagenote/shared/lib/@types/data";
-import extApi from "@pagenote/shared/lib/generateApi";
-import dayjs from 'dayjs';
 import BlogLayout from "../../layouts/blog";
 import Breadcrumbs from "../../components/Breadcrumbs";
 
@@ -9,7 +5,7 @@ export default function () {
 
     return(
         <BlogLayout post={{excerpt: '与PAGENOTE通信',title: '开发你自己的数据管理页面'}}>
-            <Breadcrumbs id={''} />
+            <Breadcrumbs />
         </BlogLayout>
     )
 }
