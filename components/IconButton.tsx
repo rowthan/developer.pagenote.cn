bb4d710fdb9f602c739a09ce682394a6e1cd04c7
rowthan <12638456+rowthan@users.noreply.github.com>
2023-01-02 00:00:34 +0800

    feat: icon button

---
+++ components/IconButton.tsx
@@ -0,0 +1,13 @@
+import {ReactElement, ReactSVGElement} from "react";
+
+export default function IconButton(props:{disabled?: boolean,children: ReactElement ,onClick: ()=>void, className?: string,[key:string]:any}) {
+    const {onClick,children,disabled, className,...left} = props;
+    return(
+        <button {...left}
+                disabled={disabled}
+                onClick={onClick}
+                className={`disabled:opacity-50 disabled:cursor-not-allowed p-1 bg-white rounded hover:bg-gray-200 shadow ${className}`}>
+            {children}
+        </button>
+    )
+}
