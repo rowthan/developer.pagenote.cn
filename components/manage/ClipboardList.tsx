2a8301003b1aedb573d06d676301bffa44b541b1
rowthan <12638456+rowthan@users.noreply.github.com>
2023-01-02 17:18:10 +0800

    refactor: tip icon

--- components/manage/ClipboardList.tsx
+++ components/manage/ClipboardList.tsx
@@ -7,6 +7,7 @@ import {toast} from "../../utils/toast";
 import useClipboardSync from "../../hooks/useClipboardSync";
 import {Pagination} from "@pagenote/shared/lib/@types/database";
 import {boxroom} from "@pagenote/shared/lib/extApi";
+import TipSvg from 'assets/svg/info.svg'
 import BoxItem = boxroom.BoxItem;
 
 export default function ClipboardList() {
@@ -65,7 +66,7 @@ export default function ClipboardList() {
             <div className='mx-auto p-3'>
                 <div className="alert shadow-lg">
                     <div>
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" className="stroke-info flex-shrink-0 w-6 h-6"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"></path></svg>
+                        <TipSvg width={32} height={32} />
                         <div>
                             <h3 className="font-bold">
                                 {
