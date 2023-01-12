f729b23d8b4da008ceeb2404d907f35523c80806
rowthan <12638456+rowthan@users.noreply.github.com>
2023-01-12 22:13:50 +0800

    feat: add checkDomain

---
+++ components/check/CheckDomain.tsx
@@ -0,0 +1,17 @@
+import {ReactElement, useEffect} from "react";
+
+export default function CheckDomain(props:{origin: string,children:ReactElement}) {
+    const {children,origin} = props;
+    useEffect(function () {
+        if(origin){
+            const currentDomain = window.location.origin;
+            if(origin!==currentDomain){
+                window.location.href = window.location.href.replaceAll(currentDomain, origin)
+            }
+        }
+    },[origin])
+
+    return(
+        {children}
+    )
+}
