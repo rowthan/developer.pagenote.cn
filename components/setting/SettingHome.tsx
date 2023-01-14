764119543e33ce8f03977619b8d8fac2a89d5726
rowthan <12638456+rowthan@users.noreply.github.com>
2023-01-14 23:40:19 +0800

    feat: Firefox 特殊处理备份弹出层

--- components/setting/SettingHome.tsx
+++ components/setting/SettingHome.tsx
@@ -1,25 +1,38 @@
 import React from "react";
 import useSettings from "../../hooks/useSettings";
 import BasicSettingLine from "./BasicSettingLine";
-
+import useWhoAmi from "../../hooks/useWhoAmi";
+import {user} from "@pagenote/shared/lib/extApi";
+import ExtensionPlatform = user.ExtensionPlatform;
+import {checkIsInPopup} from "../../utils/check";
 
 
 export default function SettingHome() {
-    const {data:setting, update:updateSetting} = useSettings();
+    const {data: setting, update: updateSetting} = useSettings();
+    const [whoAmI] = useWhoAmi();
+    const {controlC, enableType} = setting;
 
-    const { controlC,enableType } = setting;
-
+    const isFirefox = whoAmI?.extensionPlatform === ExtensionPlatform.Firefox && checkIsInPopup()
     return (
         <div className={'shadow rounded-lg'}>
-            <BasicSettingLine label={'自动启动'} subLabel={enableType==='when-needed'?'需要你手动启动，才可以开始标记内容':"网页打开就可以立即开始标记"} right={
-                <input type="checkbox" className="toggle toggle-info" checked={enableType!=='when-needed'} onChange={(e) => {
-                    updateSetting({enableType: e.target.checked ? 'always' : 'when-needed'})
-                }}/>}></BasicSettingLine>
+            <BasicSettingLine label={'自动启动'}
+                              subLabel={enableType === 'when-needed' ? '需要你手动启动，才可以开始标记内容' : "网页打开就可以立即开始标记"}
+                              right={
+                                  <input type="checkbox" className="toggle toggle-info"
+                                         checked={enableType !== 'when-needed'} onChange={(e) => {
+                                      updateSetting({enableType: e.target.checked ? 'always' : 'when-needed'})
+                                  }}/>}></BasicSettingLine>
             <BasicSettingLine label={'自动 Control C'} right={
                 <input type="checkbox" className="toggle toggle-info" checked={controlC} onChange={(e) => {
-                updateSetting({controlC: e.target.checked})
-            }}/>}/>
-            <BasicSettingLine label={'🖌画笔'} path={'/setting/light'}/>
+                    updateSetting({controlC: e.target.checked})
+                }}/>}/>
+            {
+                isFirefox ?
+                    <a target={'_blank'} href={`${whoAmI?.origin}/web/popup.html#/setting/data`}>
+                        <BasicSettingLine label={'🖌画笔'}/>
+                    </a> :
+                    <BasicSettingLine label={'🖌画笔'} path={'/setting/light'}/>
+            }
             <BasicSettingLine label={'📚数据中心'} path={'/setting/data'}/>
         </div>
     )
--- utils/check.ts
+++ utils/check.ts
@@ -4,7 +4,7 @@ export function checkIsBrowserBasicUrl(url?: string) {
     if(!url){
         return false
     }
-    return /^(edge:|extension:|chrome:|about:|chrome-extension:)/.test(url || '')
+    return /^(edge:|extension:|chrome:|about:|chrome-extension:|moz-extension:|edge-extension:)/.test(url || '')
 }
 
 export function checkIsLocalFile(url?: string) {
@@ -14,6 +14,9 @@ export function checkIsLocalFile(url?: string) {
     return /file:/.test(url)
 }
 
+export function checkIsInPopup() {
+    return window.innerWidth < 800 || window.innerHeight < 600
+}
 
 export function checkIsBrowserAppStore(platform: 'chrome'|'', url: string) {
     // https://addons.mozilla.org/zh-CN/developers/addons
