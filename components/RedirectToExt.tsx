5a39b0215e6866c474a5a5671a67aa90eb796d55
rowthan <12638456+rowthan@users.noreply.github.com>
2023-02-13 21:12:27 +0800

    fix: redirect to ext page

--- components/RedirectToExt.tsx
+++ components/RedirectToExt.tsx
@@ -4,23 +4,26 @@ import {ReactElement, useEffect} from "react";
 export default function RedirectToExt(props:{children: ReactElement}) {
     const [whoAmI] = useWhoAmi();
     useEffect(function () {
-        const origin = whoAmI?.origin;
-        if(!origin){
-            return;
-        }
-        const currentOrigin = window.location.origin;
-        const site = window.location.href.replace(currentOrigin,origin)
-        if(site){
-            const url = new URL(site);
-            let path = url.pathname;
-            if(!/^web/.test(path)){
-                path ='/web' + path;
+        const isHttp = /^http/.test(window.location.protocol);
+        if(isHttp){
+            const origin = whoAmI?.origin;
+            if(!origin){
+                return;
             }
-            if(!/html/.test(path)){
-                path += '.html'
+            const currentOrigin = window.location.origin;
+            const site = window.location.href.replace(currentOrigin,origin)
+            if(site){
+                const url = new URL(site);
+                let path = url.pathname;
+                if(!/^\/web/.test(path)){
+                    path ='/web' + path;
+                }
+                if(!/html/.test(path)){
+                    path += '.html'
+                }
+                url.pathname = path;
+                window.location.href = url.href;
             }
-            url.pathname = path;
-            window.location.href = url.href;
         }
     },[whoAmI])
     return (props.children)
