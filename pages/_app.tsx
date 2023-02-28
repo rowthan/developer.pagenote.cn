792bdf156682005b22bf33e7e09604ea7d535f65
rowthan <12638456+rowthan@users.noreply.github.com>
2023-02-28 22:40:31 +0800

    feat: font format

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -10,6 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
       <>
         <Head>
             <link rel="shortcut icon" href="https://pagenote.cn/favicon.ico" type="image/x-icon" />
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin={'crossOrigin'} />
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@100;400;700&display=swap"  rel="stylesheet" />
         </Head>
         <Script src="/components.js"/>
         <Component {...pageProps} />
