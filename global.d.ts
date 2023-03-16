fad2d854ac817a686ec28e98a2ebbe0d2463e481
rowthan <12638456+rowthan@users.noreply.github.com>
2023-03-16 21:23:22 +0800

    fix: mark-down defined

--- global.d.ts
+++ global.d.ts
@@ -4,6 +4,8 @@ declare global {
     namespace JSX {
         interface IntrinsicElements {
             'key-word': React.DetailedHTMLProps<{preview?: string} & React.HTMLAttributes<HTMLElement>, HTMLElement>;
+            'mark-down': React.DetailedHTMLProps<{css?: string} & React.HTMLAttributes<HTMLElement>, HTMLElement>;
+
         }
     }
 }
