774acc08613476d9268bbf85ec6f11b4d33984e8
rowthan <12638456+rowthan@users.noreply.github.com>
2023-04-09 11:52:36 +0800

    fix: tip for pro

--- hooks/useBooks.tsx
+++ hooks/useBooks.tsx
@@ -1,17 +1,15 @@
 import extApi from "@pagenote/shared/lib/pagenote-api";
 import useSWR from "swr";
 import dayjs from "dayjs";
-import useUserInfo from "./useUserInfo";
 
 interface Book {endTime: number, startTime: number,duration: number,remark: string,giftDays: number}
-type BookInfo = {list: Book[],expiredAt?: Date|undefined,expiredTip: string}
+type BookInfo = {list: Book[],expiredAt?: Date|undefined,expiredTip?: string}
 export default function ():[BookInfo,()=>void] {
-    const [userInfo] = useUserInfo();
     const {data={
         list:[],
         expiredAt: undefined,
         expiredTip:""
-    }} = useSWR<BookInfo>('/books',()=>fetchBookList(30 * 60),{
+    },mutate} = useSWR<BookInfo>('/books',()=>fetchBookList(30 * 60),{
         fallbackData:{
             list:[],
             expiredAt: undefined,
@@ -24,7 +22,7 @@ export default function ():[BookInfo,()=>void] {
             url:'/api/user',
             method:'GET',
             data:{
-                'query': `query{bookInfo{startTime,endTime,duration,remark,giftDays}}`
+                'query': `query{bookInfo{startTime,endTime,duration,remark,giftDays},profile{pro}}`
             },
         },{
             cacheControl:{
@@ -34,9 +32,8 @@ export default function ():[BookInfo,()=>void] {
             const data = res.data.json;
             if(Array.isArray(data.data.bookInfo)){
                 const endTime = data.data.bookInfo[0].endTime;
-                let tip = endTime ? (dayjs(endTime).isAfter(dayjs(new Date('2028-01-01'))) ? '终身' : dayjs(endTime).format('YYYY-MM-DD')) : '-';
-                const role = userInfo?.profile?.pro || 0
-                if(role>9){
+                let tip = endTime ? dayjs(endTime).format('YYYY-MM-DD') : '-';
+                if((data.data.profile?.pro || 0) > 9){
                     tip = '终身'
                 }
                 return {
