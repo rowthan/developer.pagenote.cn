55e83a7b3306178bdb5e5fd71e8dd8f75ce580b6
rowthan <12638456+rowthan@users.noreply.github.com>
2023-04-17 23:35:41 +0800

    feat: new post page

--- service/index.ts
+++ service/index.ts
@@ -72,3 +72,18 @@ export function fetchVersionDetail(version:string){
         return res?.data?.json?.data?.versionDetail || null
     })
 }
+
+
+export function getWordInfo(word:string){
+    const HOST = 'https://api.pagenote.cn'
+    return fetch(`${HOST}/api/graph/profile?`+ new URLSearchParams({
+        query: `{keyword(keyword:"${word}"){markdown}}`
+    })).then(async function(res){
+        const data = await res.json();
+        const keyword = data?.data?.keyword;
+        if(keyword.json){
+            keyword.json = JSON.parse(keyword.json)
+        }
+        return keyword;
+    })
+}
