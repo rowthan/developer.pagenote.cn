b38eba077a080c48b035ca36b84cd8b14553d080
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-08 22:52:01 +0800

    build: ignore ts error on build

--- next.config.js
+++ next.config.js
@@ -5,12 +5,17 @@ module.exports = {
   env: {
     prefix: process.env.prefix || '',
   },
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
-      use: ["@svgr/webpack"]
-    }); // 针对 SVG 的处理规则
+      use: ['@svgr/webpack'],
+    }) // 针对 SVG 的处理规则
 
-    return config;
-  }
+    return config
+  },
 }
