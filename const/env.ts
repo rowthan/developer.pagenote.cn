7f8e1aafae3b062597803c78f86a24b8f757d16a
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-10 20:06:53 +0800

    style: reset head size on mobile

--- const/env.ts
+++ const/env.ts
@@ -1,4 +1,6 @@
 export const basePath = process.env.prefix || ''
 export const DOC_API_HOST =
-  process.env.DOC_API_HOST ||
-  'https://developer-pagenote-cn-git-featnotiondoc-pagenote.vercel.app'
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:3000'
+    : process.env.DOC_API_HOST ||
+      'https://developer-pagenote-cn-git-featnotiondoc-pagenote.vercel.app'
