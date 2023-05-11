2ac1e117b9b8baf37ac5193d843a882f8fa96788
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-11 21:16:24 +0800

    style: reset notion page css

--- service/doc.ts
+++ service/doc.ts
@@ -1,17 +1,16 @@
-import { DOC_API_HOST } from "const/env"
+import { DOC_API_HOST } from 'const/env'
 import { SearchParams } from 'notion-types'
 
-export async function getNotionDocDetail(id: string){
-    const res = await fetch(`${DOC_API_HOST}/api/doc?id=${id}`)
-    const result = await res.json();
-    return {
-        props: result,
-        revalidate: 60 * 5,
-    }
+export async function getNotionDocDetail(id: string) {
+  const res = await fetch(`${DOC_API_HOST}/api/doc?id=${id}`)
+  const result = await res.json()
+  return {
+    props: result,
+    revalidate: 60 * 5,
+  }
 }
 
-export async function searchInNotion(filter: SearchParams){
-    const res = await fetch(`${DOC_API_HOST}/api/search?keyword=${filter.query}`);
-    const result = await res.json();
-    return result;
+export async function searchInNotion(filter: SearchParams) {
+  const res = await fetch(`${DOC_API_HOST}/api/search?keyword=${filter.query}`)
+  return await res.json()
 }
