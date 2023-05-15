9c5e9de45f403f6e00eadff39a1b28dd8c700569
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-15 23:56:25 +0800

    refactor: code reformat

--- pages/ext/setting.tsx
+++ pages/ext/setting.tsx
@@ -1,22 +1,22 @@
 import SettingCom from 'components/setting/Setting'
-import {HashRouter as Router, Route, Routes} from "react-router-dom";
-import CheckVersion from "../../components/check/CheckVersion";
-import React from "react";
-import BasicLayout from 'layouts/BasicLayout';
+import { HashRouter as Router, Route, Routes } from 'react-router-dom'
+import CheckVersion from 'components/check/CheckVersion'
+import React from 'react'
+import BasicLayout from 'layouts/BasicLayout'
 
-export default function Setting(){
-    return(
-        <BasicLayout nav={false} footer={true} title={'设置'} full={true}>
-            <CheckVersion requireVersion={'0.26.0'}>
-                <div className={'p-4'}>
-                    <Router>
-                        <Routes>
-                            <Route path="/" element={<SettingCom/>}/>
-                            <Route path="/setting/*" element={<SettingCom/>}/>
-                        </Routes>
-                    </Router>
-                </div>
-            </CheckVersion>
-        </BasicLayout>
-    )
+export default function Setting() {
+  return (
+    <BasicLayout nav={false} footer={true} title={'设置'} full={true}>
+      <CheckVersion requireVersion={'0.26.0'}>
+        <div className={'p-4'}>
+          <Router>
+            <Routes>
+              <Route path="/" element={<SettingCom />} />
+              <Route path="/setting/*" element={<SettingCom />} />
+            </Routes>
+          </Router>
+        </div>
+      </CheckVersion>
+    </BasicLayout>
+  )
 }
