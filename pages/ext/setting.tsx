8bbdb1bfc200a4d365a64a4b91677943e9c207a6
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-16 00:58:33 +0800

    fix: client router
    build error caused by client hash router

--- pages/ext/setting.tsx
+++ pages/ext/setting.tsx
@@ -1,10 +1,20 @@
 import SettingCom from 'components/setting/Setting'
 import { HashRouter as Router, Route, Routes } from 'react-router-dom'
 import CheckVersion from 'components/check/CheckVersion'
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import BasicLayout from 'layouts/BasicLayout'
 
 export default function Setting() {
+  const [clientLoad, setClientLoad] = useState(false)
+
+  useEffect(function () {
+    setClientLoad(true)
+  }, [])
+
+  if (!clientLoad) {
+    return null
+  }
+
   return (
     <BasicLayout nav={false} footer={true} title={'设置'} full={true}>
       <CheckVersion requireVersion={'0.26.0'}>
@@ -12,7 +22,7 @@ export default function Setting() {
           <Router>
             <Routes>
               <Route path="/" element={<SettingCom />} />
-              <Route path="*" element={<SettingCom />} />
+              <Route path="/setting/*" element={<SettingCom />} />
             </Routes>
           </Router>
         </div>
