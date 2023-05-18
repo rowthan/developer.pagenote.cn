1c7cddbd53636780e846c07a5e13e983c9fe54eb
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-18 08:30:34 +0800

    feat: check version for sign

--- hooks/useVersionValid.ts
+++ hooks/useVersionValid.ts
@@ -1,32 +1,33 @@
-import { user } from '@pagenote/shared/lib/extApi'
-import extApi from '@pagenote/shared/lib/pagenote-api'
-import useSWR from 'swr'
-import WhoAmI = user.WhoAmI
 import useWhoAmi from './useWhoAmi'
 import { useEffect, useState } from 'react'
 import { compare } from 'compare-versions'
 
-export interface VersionValid{
-    installed: boolean, 
-    valid: boolean
+export interface VersionValid {
+  installed: boolean
+  valid: boolean
 }
 
-
-export default function useVersionValid(requiredVersion='0.0.1'):VersionValid {
-   const [whoAmi] = useWhoAmi();
-   const [validInfo,setValid] = useState<VersionValid>(function(){
+export default function useVersionValid(
+  requiredVersion = '0.0.1'
+): VersionValid {
+  const [whoAmi] = useWhoAmi()
+  const [validInfo, setValid] = useState<VersionValid>(function () {
     return {
-        installed: !!whoAmi?.version,
-        valid: compare(whoAmi?.version||'',requiredVersion,'>='),
+      installed: !!whoAmi?.version,
+      valid: compare(whoAmi?.version || '0.0.0', requiredVersion, '>='),
     }
-   })
-  
-   useEffect(function(){
-    setValid({
-        installed: window.location.protocol.indexOf('http') === -1 || !!whoAmi?.version,
-        valid: compare(whoAmi?.version||'',requiredVersion,'>='),
-    })
-   },[requiredVersion, whoAmi?.version])
+  })
+
+  useEffect(
+    function () {
+      setValid({
+        installed:
+          window.location.protocol.indexOf('http') === -1 || !!whoAmi?.version,
+        valid: compare(whoAmi?.version || '0.0.0', requiredVersion, '>='),
+      })
+    },
+    [requiredVersion, whoAmi?.version]
+  )
 
   return validInfo
 }
