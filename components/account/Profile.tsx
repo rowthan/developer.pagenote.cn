ce451725eed75ced1ac82a730fb68296d63c831b
rowthan <12638456+rowthan@users.noreply.github.com>
2023-05-30 19:59:32 +0800

    feat: check account part

--- components/account/Profile.tsx
+++ components/account/Profile.tsx
@@ -42,108 +42,119 @@ export default function Profle() {
   }
 
   return (
-    <CheckVersion requireVersion={'0.26.3.10'}>
-      <div className="container mx-auto my-5 p-5">
-        <div className="md:flex no-wrap md:-mx-2 ">
-          <div className="w-full md:w-3/12 md:mx-2">
-            <div className="bg-white text-gray-950 p-3 border-t-4 border-green-400">
-              <div className="image overflow-hidden relative group">
-                <div
-                  className={'h-auto w-full mx-auto aspect-w-16 aspect-h-16'}
-                >
-                  <CheckUser
-                    fallback={
-                      <div>
-                        请
-                        <a href="/signin.html" className={'link'}>
-                          登录
-                        </a>
-                        后查看
-                      </div>
-                    }
-                  >
-                    {/* eslint-disable-next-line @next/next/no-img-element */}
-                    <img
-                      className="w-full h-full mx-auto  object-cover"
-                      src={newImg || img}
-                      alt=""
-                    />
-                  </CheckUser>
-                </div>
-                <div
-                  className={
-                    'absolute w-full -bottom-10 group-hover:bottom-0 duration-150 transition-all'
+    <div className="container mx-auto my-5 p-5">
+      <div className="md:flex no-wrap md:-mx-2 ">
+        <div className="w-full md:w-3/12 md:mx-2">
+          <div className="bg-white text-gray-950 p-3 border-t-4 border-green-400">
+            <div className="image overflow-hidden relative group">
+              <div className={'h-auto w-full mx-auto aspect-w-16 aspect-h-16'}>
+                <CheckUser
+                  fallback={
+                    <div>
+                      请
+                      <a href="/signin.html" className={'link'}>
+                        登录
+                      </a>
+                      后查看
+                    </div>
                   }
                 >
-                  <UploadTrigger
-                    onChange={(url) => {
-                      onChange({ avatar: url })
-                    }}
-                  >
-                    <div
-                      className={'w-full bg-gray-300 text-white text-center'}
-                    >
-                      替换
-                    </div>
-                  </UploadTrigger>
-                </div>
+                  {/* eslint-disable-next-line @next/next/no-img-element */}
+                  <img
+                    className="w-full h-full mx-auto  object-cover"
+                    src={newImg || img}
+                    alt=""
+                  />
+                </CheckUser>
               </div>
-              <button
-                data-tip={'点击修改'}
-                onClick={() => {
-                  setProfileModal(true)
-                }}
-                className="text-gray-900 tooltip font-bold text-xl leading-8 my-1"
+              <div
+                className={
+                  'absolute w-full -bottom-10 group-hover:bottom-0 duration-150 transition-all'
+                }
               >
-                {profile?.nickname}
-              </button>
-              <Modal
-                open={openProfileModal}
-                toggleOpen={(open) => {
-                  setProfileModal(open)
-                  refresh()
-                }}
-              >
-                <UserInfoForm onChange={onChange} />
-              </Modal>
+                <UploadTrigger
+                  onChange={(url) => {
+                    onChange({ avatar: url })
+                  }}
+                >
+                  <div className={'w-full bg-gray-300 text-white text-center'}>
+                    替换
+                  </div>
+                </UploadTrigger>
+              </div>
+            </div>
+            <button
+              data-tip={'点击修改'}
+              onClick={() => {
+                setProfileModal(true)
+              }}
+              className="text-gray-900 tooltip font-bold text-xl leading-8 my-1"
+            >
+              {profile?.nickname}
+            </button>
+            <Modal
+              open={openProfileModal}
+              toggleOpen={(open) => {
+                setProfileModal(open)
+                refresh()
+              }}
+            >
+              <UserInfoForm onChange={onChange} />
+            </Modal>
 
-              <ul className="bg-gray-100 text-gray-700  hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
-                <li className="flex items-center py-3">
-                  <span>当前版本</span>
-                  <span className="ml-auto">
-                    <DeviceInfo />
-                  </span>
-                </li>
-                <li className="flex items-center py-3 justify-between">
-                  <span>
-                    <key-word>VIP</key-word>有效期
-                  </span>
+            <ul className="bg-gray-100 text-gray-700  hover:shadow py-2 px-3 mt-3 divide-y rounded shadow-sm">
+              <li className="flex items-center py-3">
+                <CheckVersion
+                  requireVersion={'0.0.0'}
+                  fallback={<div>请安装 PAGENOTE 后查看</div>}
+                >
+                  <>
+                    <span>当前版本</span>
+                    <span className="ml-auto">
+                      <DeviceInfo />
+                    </span>
+                  </>
+                </CheckVersion>
+              </li>
+              <li className="flex items-center py-3 justify-between">
+                <span>
+                  <key-word>VIP</key-word>有效期
+                </span>
+                <CheckUser
+                  fallback={
+                    <div className={'text-sm link'}>
+                      <a href="/signin.html">登录后查看</a>
+                    </div>
+                  }
+                >
                   <BookDetail>
                     <span className="ml-auto link">{bookInfo.expiredTip}</span>
                   </BookDetail>
-                </li>
-              </ul>
-            </div>
-            <div className="my-4 ">
+                </CheckUser>
+              </li>
+            </ul>
+          </div>
+          <div className="my-4 ">
+            <CheckUser fallback={<div></div>}>
               <AuthList />
-            </div>
-
-            {/*<div className="bg-white p-3 hover:shadow">*/}
-            {/*    <InviteGroup />*/}
-            {/*</div>*/}
+            </CheckUser>
           </div>
 
-          <div className="w-full md:w-9/12 mx-2 h-64">
-            <div className="flex bg-white p-3 shadow-sm rounded-sm min-h-16">
-              <Day />
-              <div className={'ml-2'}>
-                <h2>今日相关</h2>
-                <TodayRelated />
-              </div>
+          {/*<div className="bg-white p-3 hover:shadow">*/}
+          {/*    <InviteGroup />*/}
+          {/*</div>*/}
+        </div>
+
+        <div className="w-full md:w-9/12 mx-2 h-64">
+          <div className="flex bg-white p-3 shadow-sm rounded-sm min-h-16">
+            <Day />
+            <div className={'ml-2'}>
+              <h2>今日相关</h2>
+              <TodayRelated />
             </div>
           </div>
         </div>
       </div>
-    </CheckVersion>
+    </div>
   )
 }
