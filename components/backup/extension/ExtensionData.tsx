2fa824126c864b28dfbce70a9c27a861a3d287a6
rowthan <12638456+rowthan@users.noreply.github.com>
2023-06-21 23:13:58 +0800

    Update 404.tsx

--- components/backup/extension/ExtensionData.tsx
+++ components/backup/extension/ExtensionData.tsx
@@ -1,15 +1,13 @@
-import React, { ChangeEvent, useState } from 'react'
+import React from 'react'
 import StorageInfo from './StorageInfo'
 import BasicSettingLine from '../../setting/BasicSettingLine'
 import ImportAndExport from './ImportAndExport'
 
 export default function ExtensionData() {
   return (
-    <>
-      <div className={'mb-4'}>
-        <StorageInfo />
-        <BasicSettingLine label={'管理插件数据'} right={<ImportAndExport />} />
-      </div>
-    </>
+    <div className={'mb-4'}>
+      <StorageInfo />
+      <BasicSettingLine label={'管理插件数据'} right={<ImportAndExport />} />
+    </div>
   )
 }
