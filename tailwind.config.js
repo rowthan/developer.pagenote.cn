32364b8883d78bdb0c79c1686def83b4d74d5af1
rowthan <12638456+rowthan@users.noreply.github.com>
2023-09-19 21:25:58 +0800

    feat: tailwind theme config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,7 @@ module.exports = {
     darkMode: ["media"],
     content: [
         './pages/**/*.{ts,tsx}',
+        './@/components/**/*.{ts,tsx}',
         './components/**/*.{ts,tsx}',
         './app/**/*.{ts,tsx}',
         './src/**/*.{ts,tsx}',
