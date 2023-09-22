d1c952436b6bbda3cd0aee328c178ab7bcc5ad24
rowthan <12638456+rowthan@users.noreply.github.com>
2023-09-22 09:20:02 +0800

    Update Signin.tsx

--- components/account/Signin.tsx
+++ components/account/Signin.tsx
@@ -1,28 +1,26 @@
 import SigninPart from './sign/Signin'
 import useUserInfo from '../../hooks/useUserInfo'
-import { useEffect } from 'react'
+import {useEffect} from 'react'
+import AuthList from "./AuthList";
 
 export default function Signin() {
-  const [user] = useUserInfo()
+    const [user] = useUserInfo()
 
-  const signed = !!user?.profile?.nickname
+    const signed = !!user?.profile?.nickname
 
-  useEffect(
-    function () {
-      if (signed) {
-        window.location.href = '/account'
-      }
-    },
-    [signed]
-  )
-
-  return (
-    <section className="border-red-500  min-h-fill flex items-center justify-center">
-      <div className="border p-5 m-2  rounded-2xl shadow-lg min-w-[300px] max-w-3xl items-center">
-        <div className=" px-5">
-          <SigninPart />
+    if (signed) {
+        return <div className={'m-auto pt-20 max-w-lg'}>
+            <AuthList/>
         </div>
-      </div>
-    </section>
-  )
+    }
+
+    return (
+        <section className="border-red-500  min-h-fill flex items-center justify-center">
+            <div className="border p-5 m-2  rounded-2xl shadow-lg min-w-[300px] max-w-3xl items-center">
+                <div className=" px-5">
+                    <SigninPart/>
+                </div>
+            </div>
+        </section>
+    )
 }
