356a3a51e6fc6af210f486f404302eb8062b74ee
rowthan <12638456+rowthan@users.noreply.github.com>
2023-10-06 11:14:52 +0800

    :arrow_up: upgrade shared sdk

--- hooks/useTabPagenoteState.ts
+++ hooks/useTabPagenoteState.ts
@@ -1,15 +1,36 @@
-import useSWR from "swr";
-import {fetchStatus} from "../utils/popup";
+import useSWR from 'swr'
+import extApi from '@pagenote/shared/lib/pagenote-api'
+
+function fetchStatus(tabId?: number) {
+  // @ts-ignore
+  return extApi.developer
+    .requestFront({
+      type: 'fetchStatus',
+      params: undefined,
+      header: {
+        targetTabId: tabId,
+        timeout: 2000,
+      },
+    })
+    .then(function (res) {
+      return res.data as TabState
+    })
+}
 
 type TabState = {
-    connected: false,
-    active: false,
-    enabledCopy: false,
+  connected: false
+  active: false
+  enabledCopy: false
 }
-export default function useTabPagenoteState(tabId?: number):[TabState | undefined,()=>void,boolean] {
-    const {data,mutate,isLoading} = useSWR<TabState>(`/tab/state/${tabId}`,function () {
-        return fetchStatus(tabId)
-    });
+export default function useTabPagenoteState(
+  tabId?: number
+): [TabState | undefined, () => void, boolean] {
+  const { data, mutate, isLoading } = useSWR<TabState>(
+    `/tab/state/${tabId}`,
+    function () {
+      return fetchStatus(tabId)
+    }
+  )
 
-    return [data,mutate,isLoading]
+  return [data, mutate, isLoading]
 }
