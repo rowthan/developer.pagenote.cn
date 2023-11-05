6df53f13db1fe39888cf322ef1c19e45f6f7f0d2
rowthan <12638456+rowthan@users.noreply.github.com>
2023-11-06 00:27:16 +0800

    Update close-on-installed.tsx

--- pages/widget/close-on-installed.tsx
+++ pages/widget/close-on-installed.tsx
@@ -3,21 +3,30 @@ import useVersionValid from 'hooks/useVersionValid'
 import extApi from '@pagenote/shared/lib/pagenote-api'
 import useCurrentTab from 'hooks/useCurrentTab'
 import { useRouter } from 'next/router'
+import dayjs from 'dayjs'
 
 interface Props {
   children?: ReactNode
 }
 
+const LOG_CLOSE_ON_INSTALLED = 'log_close_on_installed'
 export default function CloseOnInstalled(props: Props) {
   const { children } = props
   const router = useRouter()
   const { valid } = useVersionValid(
-    router.query?.version?.toString() || '0.26.5'
+    router.query?.version?.toString() || '0.27.0'
   )
   const { tab } = useCurrentTab()
+
   useEffect(
     function () {
-      if (router.query && valid && tab?.id) {
+      const flag = localStorage.getItem(LOG_CLOSE_ON_INSTALLED)
+      // 一天最多只提示一次
+      const today = dayjs().format('YYYY-MM-DD')
+      const closeMe = flag === today || (router.query && valid && tab?.id)
+
+      if (closeMe) {
+        localStorage.setItem(LOG_CLOSE_ON_INSTALLED, today)
         extApi.developer.chrome({
           namespace: 'tabs',
           type: 'remove',
