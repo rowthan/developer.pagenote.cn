e0f14160855c6f4ff9d9e9f253279c050b2c332e
rowthan <12638456+rowthan@users.noreply.github.com>
2023-11-13 20:38:12 +0800

    feat: did info

--- components/popup/Bookmark.tsx
+++ components/popup/Bookmark.tsx
@@ -39,37 +39,4 @@ export function Bookmark() {
     </div>
   )
 
-  return (
-    <div className={'border-none rounded-md shadow-none !bg-none'}>
-      <CardHeader className="grid grid-cols-[1fr_110px] items-start gap-4 space-y-0">
-        <div className="space-y-1">
-          <CardTitle className={'flex items-center'}>
-            <Avatar className={'w-6 h-6'}>
-              <AvatarImage src={tab?.favIconUrl} alt={tab?.title} />
-              <AvatarFallback>{tab?.title}</AvatarFallback>
-            </Avatar>
-            <span className={'ml-2'}>{tab?.title || '--'}</span>
-          </CardTitle>
-          <CardDescription>
-            {/*@ts-ignore*/}
-            {content?.description || ''}
-          </CardDescription>
-        </div>
-        <DisableButton />
-      </CardHeader>
-      {/*<CardContent>*/}
-      {/*  <div className="flex space-x-4 text-sm text-muted-foreground">*/}
-      {/*    <div className="flex items-center">*/}
-      {/*      <CircleIcon className="mr-1 h-3 w-3 fill-sky-400 text-sky-400" />*/}
-      {/*      TypeScript*/}
-      {/*    </div>*/}
-      {/*    <div className="flex items-center">*/}
-      {/*      <StarIcon className="mr-1 h-3 w-3" />*/}
-      {/*      20k*/}
-      {/*    </div>*/}
-      {/*    <div>Updated April 2023</div>*/}
-      {/*  </div>*/}
-      {/*</CardContent>*/}
-    </div>
-  )
 }
