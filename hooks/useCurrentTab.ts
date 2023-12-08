47ae0e1a0ae334d32e9c994b8f2b25d199143ce8
rowthan <12638456+rowthan@users.noreply.github.com>
2023-12-09 00:01:17 +0800

    refactor: keep last tabInfo

--- hooks/useCurrentTab.ts
+++ hooks/useCurrentTab.ts
@@ -4,45 +4,58 @@ import Tab = chrome.tabs.Tab;
 
 type TabGroups = Tab[];
 type WindowMap = Map<number, TabGroups>
+let lastTab: undefined | Tab = undefined
 export default function useCurrentTab(tabId?: number):{tab: Tab | undefined, windows: TabGroups[] | undefined} {
-    const {data: tab} = useSWR<Tab | undefined>(`/tab/currentTab/${tabId}`,getTabInfo,)
-    const {data: windowTabs} = useSWR<TabGroups[]>(`/tab/windows/${tabId}`,getAllWindows,{
-        fallbackData: []
+  const { data: tab } = useSWR<Tab | undefined>(
+    `/tab/currentTab/${tabId}`,
+    getTabInfo
+  )
+  const { data: windowTabs } = useSWR<TabGroups[]>(
+    `/tab/windows/${tabId}`,
+    getAllWindows,
+    {
+      fallbackData: [],
+    }
+  )
+
+  function getTabInfo() {
+    if (tabId) {
+      return extApi.developer
+        .chrome({
+          type: 'get',
+          namespace: 'tabs',
+          args: [tabId],
+        })
+        .then(function (res) {
+          return res.data as Tab
+        })
+    }
+
+    return extApi.developer
+      .chrome({
+        type: 'query',
+        namespace: 'tabs',
+        args: [{ active: true, lastFocusedWindow: true }],
+      })
+      .then(function (res) {
+        lastTab = (res.data || [])[0] || lastTab
+        return lastTab
+      })
+  }
+
+  function getAllWindows() {
+    return extApi.commonAction.queryTabs({}).then(function (res) {
+      const windowMap: WindowMap = new Map<number, TabGroups>()
+      res.data.forEach(function (item) {
+        const newTabs = (windowMap.get(item.windowId) || []).concat(item)
+        windowMap.set(item.windowId, newTabs)
+      })
+      return Array.from(windowMap.values())
     })
+  }
 
-    function getTabInfo() {
-        if(tabId){
-            return extApi.developer.chrome({
-                type: 'get',
-                namespace: 'tabs',
-                args: [tabId]
-            }).then(function (res) {
-                return res.data as Tab
-            })
-        }
-
-        return extApi.developer.chrome({
-            type: 'query',
-            namespace: 'tabs',
-            args:[{active: true, currentWindow: true}]
-        }).then(function (res) {
-            return (res.data || [])[0]
-        })
-    }
-
-    function getAllWindows() {
-        return extApi.commonAction.queryTabs({}).then(function (res) {
-            const windowMap: WindowMap = new Map<number, TabGroups>()
-            res.data.forEach(function (item) {
-                const newTabs = (windowMap.get(item.windowId) || []).concat(item)
-                windowMap.set(item.windowId,newTabs)
-            })
-            return Array.from(windowMap.values());
-        })
-    }
-
-    return {
-        tab: tab,
-        windows:windowTabs,
-    }
+  return {
+    tab: tab,
+    windows: windowTabs,
+  }
 }
